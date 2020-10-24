711c9ef6134a30c2ecae561b0b64f5c6d97c113d
Amey <amey8976@gmail.com>
2020-10-24 19:04:22 +0530

    chore: Added names to URLs

--- connect4/game_api/urls.py
+++ connect4/game_api/urls.py
@@ -4,8 +4,8 @@ from .views import start_game, make_move, list_moves, GamesView
 
 urlpatterns = [
     # path('', GameController.as_view())
-    path('start_game', start_game),
-    path('make_move', make_move),
-    path('list_moves/<str:game_id>', list_moves),
-    path('list_games', GamesView.as_view())
+    path('start_game', start_game, name='start_game'),
+    path('make_move', make_move, name='make_move'),
+    path('list_moves/<str:game_id>', list_moves, name='list_moves'),
+    path('list_games', GamesView.as_view(), name='list_games')
 ]
